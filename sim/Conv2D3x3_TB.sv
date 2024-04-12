1f689dd34933deba680885174467342543f286c5
JonathanQ-Maker <jonathanqfxw@gmail.com>
2024-04-11 17:33:50 -0700

    refactored Conv2D3x3 testbench

--- sim/Conv2D3x3_TB.sv
+++ sim/Conv2D3x3_TB.sv
@@ -8,22 +8,23 @@ module Conv2D3x3_TB;
     localparam WIDTH        = 8;
     localparam WORD_WIDTH   = 8; 
     localparam FILTERS      = 8;
-    localparam KERNEL_BUF_WIDTH = 64;
+    localparam KERNEL_BUF_WIDTH = 32;
 
-    localparam DEPTH        = IN_HEIGHT*IN_WIDTH*IN_CHANNEL + 8;
-    localparam KERNEL_DEPTH = 3*3*FILTERS*IN_CHANNEL*WORD_WIDTH/KERNEL_BUF_WIDTH;
+    localparam NUM_WORDS        = IN_HEIGHT*IN_WIDTH*IN_CHANNEL;
+    localparam KERNEL_DEPTH     = 3*3*FILTERS*IN_CHANNEL*WORD_WIDTH/KERNEL_BUF_WIDTH;
+    localparam FILTER_PER_LINE  = KERNEL_BUF_WIDTH / WIDTH;
 
-    reg r_aclk = 1'b0; 
-    reg r_aresetn = 1'b0;
-    reg r_tvalid = 1'b0;
-    reg [WORD_WIDTH-1:0] r_tdata = 0;
-    reg r_kernel_tvalid = 1'b0;
-    reg [63:0] r_kernel_tdata = 64'h0101010101010101;
+    reg r_aclk                      = 0; 
+    reg r_aresetn                   = 1;
+    reg r_tvalid                    = 0;
+    reg [WORD_WIDTH-1:0] r_tdata    = 0;
+    reg r_kernel_tvalid             = 0;
+    reg [63:0] r_kernel_tdata       = 64'h0101010101010101;
 
     wire w_tready;
     wire w_kernel_tready;
     wire w_tvalid;
-    wire [WORD_WIDTH*8-1:0] w_tdata;
+    wire [FILTER_PER_LINE*WORD_WIDTH-1:0] w_tdata;
 
     Conv2D3x3 #(
         .IN_HEIGHT(IN_HEIGHT), 
@@ -49,41 +50,72 @@ module Conv2D3x3_TB;
             .o_tdata(w_tdata)
         );
 
-    // 50mhz clock
-    always #20 r_aclk <= !r_aclk;
+    // 100mhz clock
+    always #5 r_aclk <= !r_aclk;
 
-    task reset();
+    task reset_UUT();
         @(posedge r_aclk);
-        r_aresetn <= 1'b0;
+        r_aresetn <= 0;
         @(posedge r_aclk);
-        r_aresetn <= 1'b1;
-        @(posedge r_aclk);
+        r_aresetn <= 1;
         @(posedge r_aclk);
     endtask
 
-    initial begin
-        reset();
-
+    task fill_kernel();
         r_kernel_tvalid <= 1;
-        repeat(KERNEL_DEPTH) begin
+        for (int i = 0; i < KERNEL_DEPTH; i++) begin
+            if (w_kernel_tready != 1) // check that kernel is accepting
+                $error("Fill Kernel[%d]: EXPECTED w_kernel_tready == 1 but got %d", i, w_kernel_tready);
             @(posedge r_aclk);
         end
-
-        repeat(DEPTH*2) begin
-            r_tvalid <= 1'b1;
-            @(posedge r_aclk);
-            if (r_tvalid && w_tready) begin
-                r_tvalid <= 1'b0;
-                if (r_tdata == 31)
-                    r_tdata <= 0;
-                else
-                    r_tdata <= r_tdata + 1;
-            end
-        end
-        r_tvalid <= 0;
-
-        repeat(10)
+        r_kernel_tvalid <= 0;
         @(posedge r_aclk);
+    endtask
+
+    task fill_input();
+        r_tdata <= 0;
+        @(posedge r_aclk);
+        while (r_tdata != NUM_WORDS-1) begin
+            r_tvalid <= 1;
+            @(posedge r_aclk);
+            while (!w_tready) begin
+                @(posedge r_aclk);
+            end
+            r_tdata <= r_tdata + 1;
+            r_tvalid <= 0;
+        end
+    endtask
+
+    initial begin
+        reset_UUT();
+
+        // After reset checks. 
+        if (w_tready != 1)
+            $error("EXPECTED w_tready == 1 but got %d", w_tready); 
+
+        if (w_kernel_tready != 1)
+            $error("EXPECTED w_kernel_tready == 1 but got %d", w_kernel_tready);
+
+        if (w_tvalid != 0)
+            $error("EXPECTED w_tvalid == 0 but got %d", w_tvalid); 
+        
+        // Fill kernel
+        fill_kernel();
+
+        // Assert that kernel is no longer accepting
+        if (w_kernel_tready != 0)
+            $error("LEXPECTED w_kernel_tready == 0 but got %d", w_kernel_tready);
+
+        // fill the input image twice
+        fill_input();
+        fill_input();
+
+        // Assert that UUT is ready for new image
+        if (w_tready != 1)
+            $error("EXPECTED w_tready == 1 but got %d", w_tready);
+
+        // some exit padding
+        repeat(10) @(posedge r_aclk);
 
         $finish();
     end
